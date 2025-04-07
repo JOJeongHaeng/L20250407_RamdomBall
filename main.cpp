7b2c9a0e73f09bdf47d8cee0838756bf187bfe1e
JeongHaeng Cho <118785061+JOJeongHaeng@users.noreply.github.com>
2025-04-07 10:51:50 +0900

    Update main.cpp
    52개의 공 중복 없이 뽑기

--- main.cpp
+++ main.cpp
@@ -1,17 +1,47 @@
 #include <iostream>
 #include <cstdlib>
+#define POCKET_SIZE	52
+
 
 using namespace std;
 
-int RandomRangeDemo(int RangeMin, int RangeMax) {
-	int Used[52] = { 0, };
 
-	int r = ((double)rand() / RAND_MAX) * (RangeMax - RangeMin) + RangeMin;
-	return r;
+
+int main() 
+{
+	srand((unsigned int)time(NULL));
+	int Pocket[POCKET_SIZE] = { 0, };
+
+	for (int Index = 0; Index < POCKET_SIZE; Index++) 
+	{
+		Pocket[Index] = Index+1;
+	}
+
+	int Temp;
+	for (int Count = 0; Count < POCKET_SIZE *100; Count++) 
+	{
+		int First = rand() % POCKET_SIZE;
+		int Second = rand() % POCKET_SIZE;
+		Temp = Pocket[First];
+		Pocket[First] = Pocket[Second];
+		Pocket[Second] = Temp;
+	}
+	
+	for (int Index = 0; Index < POCKET_SIZE; Index++) 
+	{
+		cout << Pocket[Index] << ", ";
+	}
+
 }
 
-int main() {
-	int n;
+//int RandomRangeDemo(int RangeMin, int RangeMax) {
+//	int Used[52] = { 0, };
+//
+//	int r = ((double)rand() / RAND_MAX) * (RangeMax - RangeMin) + RangeMin;
+//	return r;
+//}
+
+/*int n;
 	cout << "몇개를 뽑으시겠습니까?" << endl;
 	cin >> n;
 	bool* Used = (bool*)calloc(52, sizeof(bool));
@@ -25,5 +55,4 @@ int main() {
 		else {
 			i--;
 		}
-	}
-}
+	}*/
